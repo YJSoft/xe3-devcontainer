ab0228e7169e1ee35d82a559d2f65cee3af68f58
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2022-12-20 01:38:56 +0000

    Improve lighttpd rewrite rule

--- .devcontainer/lighttpd/init.sh
+++ .devcontainer/lighttpd/init.sh
@@ -8,6 +8,10 @@ sudo sed -i "s/var\/www\/html/var\/www\/rhymix/g" /etc/lighttpd/lighttpd.conf
 sudo cp -f $PWD/.devcontainer/lighttpd/php.conf /etc/lighttpd/conf-available/15-fastcgi-php.conf
 sudo cp -f $PWD/.devcontainer/lighttpd/rhymix.conf /etc/lighttpd/conf-enabled/20-rhymix.conf
 
+# copy some template
+sudo cp -f $PWD/.devcontainer/lighttpd/403.html /var/www/403.html
+sudo chown www-data:www-data /var/www/403.html
+
 # enable php
 sudo lighttpd-enable-mod rewrite
 sudo lighttpd-enable-mod fastcgi
