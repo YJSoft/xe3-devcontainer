6b05a3c2d7e9378c36a5df518147c732dac03dd9
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2023-01-25 08:36:06 +0000

    add git pull for precomplied codespace

--- .devcontainer/lighttpd/start.sh
+++ .devcontainer/lighttpd/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+pushd $PWD/rhymix
+git pull
+popd
+
 sudo service mysql start
 sudo service lighttpd start
 sudo service php7.4-fpm start
--- .devcontainer/xe-core/start.sh
+++ .devcontainer/xe-core/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+pushd $PWD/xe
+git pull
+popd
+
 sudo service mysql start
 sudo service apache2 start
