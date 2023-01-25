def0626aefa3c34561dc93b786c7aa7b01c70a38
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2023-01-25 07:18:08 +0000

    Add xe core for comparing

---
+++ .devcontainer/install-xe.php
@@ -0,0 +1,18 @@
+<?php $install_config = array (
+  'db_type' => 'mysql',
+  'db_port' => '3306',
+  'db_hostname' => 'localhost',
+  'db_userid' => 'xpressengine',
+  'db_password' => 'xpressengine',
+  'db_database' => 'xpressengine',
+  'db_table_prefix' => 'xe_auto',
+  'db_charset' => 'utf8',
+  'use_rewrite' => 'Y',
+  'time_zone' => '0900',
+  'email_address' => 'admin@admin.net',
+  'password' => 'admin',
+  'password2' => 'admin',
+  'nick_name' => 'admin',
+  'user_id' => 'admin',
+  'lang_type' => 'ko',
+);
