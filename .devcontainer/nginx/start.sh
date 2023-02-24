b522e67d5ed2cc5c33c3b52befeb0a0a0c0b0f7d
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2023-02-24 01:30:19 +0000

    Revert "add git pull for precomplied codespace"
    This reverts commit 6b05a3c2d7e9378c36a5df518147c732dac03dd9.

--- .devcontainer/nginx/start.sh
+++ .devcontainer/nginx/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-pushd $PWD/rhymix
-git pull
-popd
-
 sudo service mysql start
 sudo service nginx start
 sudo service php7.4-fpm start
--- .devcontainer/xe-core/start.sh
+++ .devcontainer/xe-core/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-pushd $PWD/xe
-git pull
-popd
-
 sudo service mysql start
 sudo service apache2 start
