885550ad00ba9f57ac65de95f7c3d455ee5f7fd8
YJSoft <yjsoft@yjsoft.xyz>
2023-03-27 05:43:18 +0000

    Fix code for domain malfunction

--- .devcontainer/install-xe3.sh
+++ .devcontainer/install-xe3.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # set domain
-sudo sed -i "s/%DOMAIN%/$CODESPACE_NAME-80.preview.app.github.dev/g" $PWD/.devcontainer/install.yaml
+sed -i "s/%DOMAIN%/$CODESPACE_NAME-80.preview.app.github.dev/g" $PWD/.devcontainer/install.yaml
 cp -f $PWD/.devcontainer/install.yaml $PWD/xe3/.dev-install.yaml
+
+# fix domain problem
+sed -i '/^<?php$/a \\n$_SERVER['"'"'HTTP_HOST'"'"'] = isset($_SERVER['"'"'HTTP_X_FORWARDED_HOST'"'"']) ? $_SERVER['"'"'HTTP_X_FORWARDED_HOST'"'"'] : "localhost";' $PWD/xe3/index.php
 
 # install xe3
 pushd $PWD/xe3
